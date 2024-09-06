2a929a39d13397805e007b409e4e393c3d043ef6
Tyrone Johnson <tjcloa@gmail.com>
2024-09-06 14:50:17 +0300

    clean up deployment script

--- script/deploy-core.s.sol
+++ script/deploy-core.s.sol
@@ -33,23 +33,8 @@ contract CoreDeployer is Script {
         address wNative;
     }
 
-    // address[12] public quoteAssets = [
-    //     0xb5E3dbAF69A46B71Fe9c055e6Fa36992ae6b2c1A,
-    //     0x5fA95212825a474E2C75676e8D833330F261CaeD,
-    //     0xA6b5f74DDCc75b4b561D84B19Ad7FD51f0405483,
-    //     0x2267Ae86066097EF49884Aac96c63f70fE818eb3,
-    //     0x3E610F32806e09C2Ba65b8c88A6E4f777c8Cb559,
-    //     0x67bF6DE7f8d4d13FBa410CBe05219cB26242A7C9,
-    //     0xf1e7167A0b085B52A8ad02A5Cc48eD2027b8B577,
-    //     0xfCDaC6196C22908ddA4CE84fb595B1C7986346bF,
-    //     0x87d252A68a0AC2428C6e849f4Ec0b30DD3DCA62B,
-    //     0xFEbad8c0EA06e816FF21D1c772c46E02F10F2A23,
-    //     0xf83A152C0A526a45E93D91c95894a19A1258E30E,
-    //     0x5c7bEa38BD9d825212a1BCf0cCA4b9C122f6Bd00
-    // ];
-
-    //string[] chains = ["bob_testnet"];
-    string[] chains = ["anvil"];
+    // string[] chains = ["anvil"];
+    string[] chains = ["bob_testnet"];
 
     function setUp() public {
         // _overwriteDefaultArbitrumRPC();
@@ -160,6 +145,8 @@ contract CoreDeployer is Script {
             } else {
                 quoter = deployment.quoter;
             }
+            // Serialize the updated deployment struct back to JSON
+            // @dev Moving the updatedJsonBase assignment down will cause Stack too deep issue
             string memory updatedJsonBase = jsonBase.serialize(string(abi.encodePacked(".", chains[i])), abi.encode(deployment));
 
             // address[] memory quoteAssets = deployment.quoteAssets;
@@ -193,13 +180,10 @@ contract CoreDeployer is Script {
             console.log("The new pendingOwner: ", LBFactory(factoryV2).pendingOwner());
             console.log("Please accept the ownership of factory at: ", address(factoryV2));
 
-            // Serialize the updated deployment struct back to JSON
-            //bytes memory updatedDeployment = abi.encode(deployment);
-
-            // Write the updated JSON back to the file
             vm.stopBroadcast();
-            //bytes memory updatedDeployment = abi.encode(deployment);
-            // vm.writeFile("script/config/deployments.json", abi.decode(updatedJsonBase));
+            //@todo this approach messes up the json formatting - need to find the right way:
+            // Write the updated JSON back to the file
+            // vm.writeFile("script/config/deployments.json", abi.decode(updatedJsonBase)); 
         }
     }
 
